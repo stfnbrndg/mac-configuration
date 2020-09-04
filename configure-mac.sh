066835e3c79f7cb8366b9049d7f6b8875621c5ab
Stefan Brundige <stefan.brundige@kub.org>
2020-09-03 20:44:49 -0400

    init

---
+++ configure-mac.sh
@@ -0,0 +1,16 @@
+brew update
+brew install git
+brew install python3
+brew install ansible
+brew install terraform
+brew install azure-cli
+brew cask install docker
+brew cask install atom
+brew cask install alfred
+brew cask install authy
+brew cask install balenaetcher
+brew cask install powershell
+brew cask install 1password
+brew cask install spectacle
+brew cask install visual-studio-code
+brew cask install firefox
