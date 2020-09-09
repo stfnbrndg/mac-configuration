97b253ee557a103faf8dcceac061622d6b0dbf2c
brs06885 <stefan.brundige@kub.org>
2020-09-09 09:38:10 -0400

    chore: match script run method

--- configure-mac.sh
+++ configure-mac.sh
@@ -1,4 +1,4 @@
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew update
 brew install git
